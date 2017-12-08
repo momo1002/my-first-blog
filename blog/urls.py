748d0c5f2bf331a28786bfa103a9c38306b10314
momo <momoko.nakada1002@gmail.com>
2017-12-07 16:05:05 -0800

    Modified templates to display posts from database.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^ciccc/', views.ciccc, name='ciccc'),
+    # url(r'^ciccc/', views.ciccc, name='ciccc'),
 ]
